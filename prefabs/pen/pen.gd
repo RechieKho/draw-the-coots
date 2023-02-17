bfbf840eec586c1d2d00c8c3a76bde234570df45
RechieKho <the.real.richie.kho@gmail.com>
2023-02-17 19:06:48 +0800

    Add Pen tip to pen for detection.

--- prefabs/pen/pen.gd
+++ prefabs/pen/pen.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 signal on_stroke(point)
 
+onready var pen_tip := $PenTip
+
 var is_grabbing := false
 var can_grab := false
 
@@ -9,9 +11,13 @@ var can_grab := false
 func _on_Pen_mouse_entered(): can_grab = true
 func _on_Pen_mouse_exited(): can_grab = false
 
+func set_is_grabbing(value: bool):
+	is_grabbing = value
+	pen_tip.monitorable = value
+
 func _unhandled_input(event): 
 	if event is InputEventMouseButton:
-		is_grabbing = event.pressed and can_grab
+		set_is_grabbing(event.pressed and can_grab)
 
 func _process(_delta):
 	var global_mouse_position = get_global_mouse_position()
