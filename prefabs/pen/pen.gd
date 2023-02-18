a9d2ae09a7d421cc9fb61b56259a4217f92882a5
RechieKho <the.real.richie.kho@gmail.com>
2023-02-18 14:26:26 +0800

    Add pen animation.

--- prefabs/pen/pen.gd
+++ prefabs/pen/pen.gd
@@ -3,21 +3,30 @@ extends Area2D
 signal on_stroke(point)
 
 onready var pen_tip := $PenTip
+onready var animation_player := $AnimationPlayer
 
 var is_grabbing := false
 var can_grab := false
 
 
-func _on_Pen_mouse_entered(): can_grab = true
-func _on_Pen_mouse_exited(): can_grab = false
+func _on_Pen_mouse_entered(): 
+	can_grab = true
+	animation_player.play("ACTIVE")
+
+func _on_Pen_mouse_exited(): 
+	can_grab = false
+	animation_player.play_backwards("ACTIVE")
 
 func set_is_grabbing(value: bool):
 	is_grabbing = value
 	pen_tip.monitorable = value
+	if value: animation_player.play("DRAW")
+	else: animation_player.play_backwards("DRAW")
 
 func _unhandled_input(event): 
 	if event is InputEventMouseButton:
-		set_is_grabbing(event.pressed and can_grab)
+		if can_grab:
+			set_is_grabbing(event.pressed)
 
 func _process(_delta):
 	var global_mouse_position = get_global_mouse_position()
