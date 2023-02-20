422c5dff19b50e5247ef683b7288b7ed5a2de0e3
RechieKho <the.real.richie.kho@gmail.com>
2023-02-20 14:43:07 +0800

    Add bounding box for pen.

--- prefabs/pen/pen.gd
+++ prefabs/pen/pen.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 signal on_stroke(point)
 
+export(NodePath) var paper_region_path := ""
+
 onready var pen_tip := $PenTip
 onready var animation_player := $AnimationPlayer
 onready var particles := $Particles
+onready var paper_region = get_node_or_null(paper_region_path)
 
 var is_grabbing := false
 var can_grab := false
@@ -16,26 +19,49 @@ func _on_Pen_mouse_entered():
 
 func _on_Pen_mouse_exited(): 
 	can_grab = false
-	animation_player.play_backwards("ACTIVE")
+	if not is_grabbing: animation_player.play_backwards("ACTIVE")
 
 func set_is_grabbing(value: bool):
+	if is_grabbing == value: return
 	is_grabbing = value
 	pen_tip.monitorable = value
 	if value: 
 		animation_player.play("DRAW")
 		particles.emitting = true
-	else: animation_player.play_backwards("DRAW")
+	elif can_grab: animation_player.play_backwards("DRAW")
+	else: animation_player.play_backwards("ACTIVE")
 
-func _unhandled_input(event): 
-	if event is InputEventMouseButton:
-		if can_grab:
-			set_is_grabbing(event.pressed)
+#func _unhandled_input(event): 
+#	if event is InputEventMouseButton:
+#		if can_grab:
+#			set_is_grabbing(event.pressed)
 
 func _process(_delta):
+	# inputs
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		if can_grab: set_is_grabbing(true)
+	else: set_is_grabbing(false)
+	
+	# position
 	var global_mouse_position = get_global_mouse_position()
 	if is_grabbing and (global_position - global_mouse_position).length_squared() > 0.01:
-		global_position = global_mouse_position
-		emit_signal("on_stroke", global_mouse_position)
+		var clamped_position = null
+		if paper_region is Control: 
+			clamped_position = Vector2(
+				clamp(
+					global_mouse_position.x, 
+					paper_region.rect_position.x,
+					paper_region.rect_position.x + paper_region.rect_size.x
+				),
+				clamp(
+					global_mouse_position.y, 
+					paper_region.rect_position.y,
+					paper_region.rect_position.y + paper_region.rect_size.y
+				)
+			)
+		else: clamped_position = global_mouse_position
+		global_position = clamped_position
+		emit_signal("on_stroke", clamped_position)
 
 
 
