f7ec8e52ff090fb1ebea3ccd2a096b4cb63c0051
RechieKho <the.real.richie.kho@gmail.com>
2023-02-23 20:00:55 +0800

    Add dead scene.

---
+++ scenes/dead/dead.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+
+func _on_RedrawButton_button_up():
+	GFX.change_scene("res://scenes/drawing-stage/drawing-stage.tscn")
+
+
+func _on_MainMenu_button_up():
+	pass # Replace with function body.
--- scenes/drawing-stage/health-bar.gd
+++ scenes/drawing-stage/health-bar.gd
@@ -4,4 +4,4 @@ extends ProgressBar
 func reduce_health(amount):
 	value -= amount
 	if value == 0:
-		pass # Deal with death
+		GFX.change_scene("res://scenes/dead/dead.tscn")
